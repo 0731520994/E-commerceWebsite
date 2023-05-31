03a621ea39751ec6fd996a98c8475cf5934e3917
Mongy <mongidlanjwa@gmail.com>
2023-05-31 11:26:34 +0200

    fewChanges

--- script/code.js
+++ script/code.js
@@ -1,13 +1,22 @@
 let product =[
     {
-        id: '',
-        name: '',
-
-    },
+        id: '1',
+        image:'../Images/iphone13.webp',
+        name: 'iphone13',
+        price: 'R27000'
 
     {
-        id: 2
-        
+        id: '2'
+        image: '../Images/airpods.jpeg'
+        name: 'airpods'
+        price: '700'
+    }
+
+    {
+        id: '3'
+        name: 'charger'
+        image: '../Images/charger.webp'
+        price: 'R500'
     }
 ]
 
